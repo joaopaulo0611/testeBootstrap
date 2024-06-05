d22f09cc6dfc913d0458c70e64bbd4bd083b2552
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-05 12:49:09 -0300

    salvo

---
+++ login.js
@@ -0,0 +1,18 @@
+let estudante = document.getElementById('estudante')
+let bilbiotecario = document.getElementById('biblio')
+
+function redirecionar() {
+    if(estudante.checked){
+        window.location.href = 'pagina_estudante.html';
+    } else if(bilbiotecario.checked){
+        window.location.href = 'pagina_bibliotecario.html';
+    }else{
+        alert("Por favor, selecione uma opção.")
+    }
+}
+
+
+
+
+
+
